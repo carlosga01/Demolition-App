e08caba00dab09dc46d1bdf0e0b45f93d18a3151
Carlos Garcia <carlosga@mit.edu>
2018-04-30 19:41:21 -0400

    added a controller for the sign in scene

---
+++ Demolition/Demolition/SigninViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SigninViewController.swift
+//  Demolition
+//
+//  Created by Carlos Garcia on 4/30/18.
+//  Copyright © 2018 6.S062 Project. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SigninViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var teamSelector: UISegmentedControl!
+}
