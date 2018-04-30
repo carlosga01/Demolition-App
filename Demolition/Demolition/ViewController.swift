a270439992e1bd7f741506366d37cefeea459a40
Carlos Garcia <carlosga@mit.edu>
2018-04-30 01:24:44 -0400

    trying the blutooth connection

--- Demolition/Demolition/ViewController.swift
+++ Demolition/Demolition/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
-
+    
+    var centralManager: CBCentralManager?
+    var peripheralManager = CBPeripheralManager()
+    
+    let mainCellReuseIdentifier = "MainCell"
+    let columnCount = 2
+    let margin : CGFloat = 10
+    var visibleDevices = Array<Device>()
+    var cachedDevices = Array<Device>()
+    var cachedPeripheralNames = Dictionary<String, String>()
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
+        
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func fireButton(_ sender: UIButton) {
+        
+        self.centralManager?.scanForPeripherals(withServices: [Constants.SERVICE_UUID], options: [CBCentralManagerScanOptionAllowDuplicatesKey : true])
+        print("im scanning")
+    }
+    
+}
 
+extension ViewController : CBPeripheralManagerDelegate {
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        
+        if (peripheral.state == .poweredOn){
+            
+            let adData = "hello"
+            peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey : [Constants.SERVICE_UUID], CBAdvertisementDataLocalNameKey: adData])
+        }
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
+        
+        for request in requests {
+            if let value = request.value {
+                // HANDLE CONNECTION
+                print("hit")
+            }
+            self.peripheralManager.respond(to: request, withResult: .success)
+        }
+    }
+}
+
+extension ViewController:CBCentralManagerDelegate{
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if (central.state == .poweredOn){
+            print("state is on")
+            
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        print(peripheral)
+        print(advertisementData)
+        
+    }
 }
 
