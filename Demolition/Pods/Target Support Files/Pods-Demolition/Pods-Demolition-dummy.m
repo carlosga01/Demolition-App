d874206254c3c8e5dfd33ade5b098520a1b4a7fd
Omar Gonzalez <omargonzalez@dhcp-18-189-40-164.dyn.MIT.EDU>
2018-05-02 21:00:03 -0400

    added Firebase dependencies

---
+++ Demolition/Pods/Target Support Files/Pods-Demolition/Pods-Demolition-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Demolition : NSObject
+@end
+@implementation PodsDummy_Pods_Demolition
+@end
---
+++ Demolition/Pods/Target Support Files/Pods-DemolitionUITests/Pods-DemolitionUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemolitionUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DemolitionUITests
+@end
